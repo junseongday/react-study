e5d3280971b0399a99937d43ed8cb4196fdb264a
junseong <junseongday@gmail.com>
2021-02-10 11:13:11 +0900

    Context API 를 활용한 상태 관리

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -4,6 +4,7 @@ import TodoCreate from './components/TodoCreate';
 import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
+import { TodoProvider } from './TodoContext';
 
 const GloabalStyle = createGlobalStyle`
   body {
@@ -12,14 +13,14 @@ const GloabalStyle = createGlobalStyle`
 `;
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GloabalStyle/>
       <TodoTemplate>
         <TodoHead />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
--- mashup-todolist/src/components/TodoHead.js
+++ mashup-todolist/src/components/TodoHead.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { useTodoState } from '../TodoContext'
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
   padding-left: 32px;
@@ -26,6 +26,8 @@ const TodoHeadBlock = styled.div`
 `;
 
 function TodoHead() {
+  const todo = useTodoState()
+  console.log(30, todo)
   return (
       <>
         <TodoHeadBlock>
@@ -36,5 +38,4 @@ function TodoHead() {
       </>
   );
 }
-
 export default TodoHead;
