d2185a3ad516d913a10128b78d5734cc34f0a709
junseongday <junseongday@gmail.com>
2021-02-15 22:19:15 +0900

    리덕스 모듈 만들기

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './exercise'
+import {createStore} from 'redux';
+import rootReducer from './modules';
+import {Provider} from 'react-redux'
+
+const store = createStore(rootReducer);
+console.log(10, store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
