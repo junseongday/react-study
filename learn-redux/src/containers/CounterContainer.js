f9d4164fef08f5d809e82851f4ec29873c734650
junseongday <junseongday@gmail.com>
2021-02-15 23:22:39 +0900

    useSelector 최적화

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector, useDispatch, shallowEqual} from 'react-redux';
 import Counter from '../components/Counter';
 import { decrease, increase, setDiff } from '../modules/counter';
 
@@ -8,7 +8,7 @@ function CounterContainer() {
     const {number, diff} = useSelector(state => ({
         number: state.counter.number,
         diff: state.counter.diff
-    }))
+    }), shallowEqual)
     const dispatch = useDispatch();
     const onIncrease = () => dispatch(increase());
     const onDecrease = () => dispatch(decrease());
