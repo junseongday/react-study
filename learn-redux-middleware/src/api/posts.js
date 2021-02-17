14491103e51bc8015c594fd6b0af623393bcde89
junseong <junseongday@gmail.com>
2021-02-17 14:50:32 +0900

    CORS 와 Webpack DevServer Proxy

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const getPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts')
+    const res = await axios.get('/posts')
     return res.data;
 }
 
 export const getPostById = async (id) => {
-    const res = await axios.get(`http://localhost:4000/posts/${id}`)
+    const res = await axios.get(`/posts/${id}`)
     return res.data;
 }
