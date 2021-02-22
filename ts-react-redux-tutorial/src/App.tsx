5c509197b61ce4b397d3deb45020585425ec3029
junseong <junseongday@gmail.com>
2021-02-23 00:39:01 +0900

    5. TypeScript 에서 리덕스 프로처럼 사용하기(2)

--- ts-react-redux-tutorial/src/App.tsx
+++ ts-react-redux-tutorial/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import CounterContainer from './containers/CounterContainer';
+import TodoApp from './containers/TodoApp';
 
 function App() {
   return (
-    <CounterContainer />
+    <TodoApp />
   );
 }
 
